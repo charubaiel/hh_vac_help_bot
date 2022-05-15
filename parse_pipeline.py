0f137d80fba12a615734c1105e320185568d6d90
Артем Александрин Home <aleksandrinay@it.mos.ru>
2022-05-15 17:00:59 +0300

    upd schema

--- parse_pipeline.py
+++ parse_pipeline.py
@@ -10,19 +10,23 @@ def parse_pages(context)->list:
     return get_hh_pages(context.op_config['search_params'])
 
 @op
-def check_duplicates(vacancy_list:list)->list:
-    return check_doppelgangers(vacancy_list,params['user'])
+def check_duplicates(context,vacancy_list:list)->list:
+    return check_doppelgangers(vacancy_list,context.op_config['search_params']['user'])
 
 @op
-def load_data(uniq_vacancy_list:list):
-    return batch_load_to_db(uniq_vacancy_list,params['user'],query=params['text'])
+def load_data(context,uniq_vacancy_list:list):
+    return batch_load_to_db(uniq_vacancy_list,context.op_config['search_params']['user'],
+                            query=context.op_config['search_params']['text'])
 
 
 @job(config= config) 
 def hh_parse_job():
     load_data(check_duplicates(parse_pages()))
 
-basic_schedule = ScheduleDefinition(job=hh_parse_job, cron_schedule=reporting['schedule_time'],execution_timezone="Europe/Moscow",default_status=DefaultScheduleStatus.RUNNING)
+basic_schedule = ScheduleDefinition(job=hh_parse_job,
+                                    cron_schedule=reporting['schedule_time'],
+                                    execution_timezone="Europe/Moscow",
+                                    default_status=DefaultScheduleStatus.RUNNING)
 
 @repository
 def hh_dagster_parse():
